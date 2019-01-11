2b4ff8b5583f1f94cc002d1b3e95ad983edc37aa
LakshmikanthRavi <46441168+LakshmikanthRavi@users.noreply.github.com>
2019-01-11 09:14:17 -0300

    Create amstrongrange.py

---
+++ amstrongrange.py
@@ -0,0 +1,13 @@
+d,j=map(int,input().split())
+li=[]
+for num in range(d, j + 1):
+  sum1=0
+  s=len(str(num))
+  y=num
+  while(y>0):
+    d=y%10
+    sum1=sum1+d**s
+    y=y//10
+  if(num==sum1):
+    li.append(num)
+print(*li)
